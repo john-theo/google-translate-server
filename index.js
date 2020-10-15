019d11004c9b4a39c01311547852e98150056e84
amedays <72491614+amedays@users.noreply.github.com>
2020-10-15 14:21:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const translate = require("google-translate-cn-api");
-// const stdio = require("stdio");
+
 const express = require("express");
-// const date = require("date-and-time");
+
 
 const app = express();
-// const ops = stdio.getopt({
-//   port: { key: "p", args: 1, description: "running port", default: 30031 },
-//   domain: {
-//     key: "d",
-//     args: 1,
-//     description: "google translate port",
-//     default: "cn",
-//   },
-// });
+
 const port = process.env.PORT || 3000;;
 const domain = "com";
 
-// log functionalities
-// const timeFormat = date.compile("YYYY/MM/DD HH:mm:ss");
-// const prettyJson = (object) =>
-//   JSON.stringify(object)
-//     .replace(/"/g, "")
-//     .replace(/([,:])/g, "$1 ");
-// function log(req, text, status) {
-//   const now = date.format(new Date(), timeFormat);
-//   let ip = req.connection.remoteAddress;
-//   const params = prettyJson(req.query);
-//   console.log(`${now} [${ip}] ${status}  "${text}"  ${params}`);
-// }
+
 
 // express server
 app.get("/", function (req, res) {
