0d2a0ee475046ee1465e63b58cc149a37912d5fb
amedays <72491614+amedays@users.noreply.github.com>
2020-10-15 14:02:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const translate = require("google-translate-cn-api");
 // const stdio = require("stdio");
 const express = require("express");
-const date = require("date-and-time");
+// const date = require("date-and-time");
 
 const app = express();
 // const ops = stdio.getopt({
@@ -17,17 +17,17 @@ const port = process.env.PORT || 3000;;
 const domain = "com";
 
 // log functionalities
-const timeFormat = date.compile("YYYY/MM/DD HH:mm:ss");
-const prettyJson = (object) =>
-  JSON.stringify(object)
-    .replace(/"/g, "")
-    .replace(/([,:])/g, "$1 ");
-function log(req, text, status) {
-  const now = date.format(new Date(), timeFormat);
-  let ip = req.connection.remoteAddress;
-  const params = prettyJson(req.query);
-  console.log(`${now} [${ip}] ${status}  "${text}"  ${params}`);
-}
+// const timeFormat = date.compile("YYYY/MM/DD HH:mm:ss");
+// const prettyJson = (object) =>
+//   JSON.stringify(object)
+//     .replace(/"/g, "")
+//     .replace(/([,:])/g, "$1 ");
+// function log(req, text, status) {
+//   const now = date.format(new Date(), timeFormat);
+//   let ip = req.connection.remoteAddress;
+//   const params = prettyJson(req.query);
+//   console.log(`${now} [${ip}] ${status}  "${text}"  ${params}`);
+// }
 
 // express server
 app.get("/", function (req, res) {
@@ -35,8 +35,8 @@ app.get("/", function (req, res) {
   delete req.query.text;
 
   translate(text, { ...{ domain: domain }, ...req.query })
-    .then((resp) => res.json(resp) && log(req, text, 200))
-    .catch((resp) => res.status(400).json(resp) && log(req, text, 400));
+    .then((resp) => res.json(resp))
+    .catch((resp) => res.status(400).json(resp));
 });
 
 app.listen(port, function () {
